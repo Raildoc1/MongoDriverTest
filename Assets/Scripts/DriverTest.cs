4a6b159fd20f04df90b84757521cb72afec2cc6e
Raildoc1 <kirill.gaiduk.personal@gmail.com>
2024-02-14 10:32:21 +0300

    Initial commit

---
+++ Assets/Scripts/DriverTest.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class DriverTest : MonoBehaviour
+    {
+        const string ConnectionString = "mongodb://localhost:27017,localhost:27019/?replicaSet=poolReplSet";
+
+        void Awake()
+        {
+            Debug.Log("Initializing...");
+
+            var mongoClient = new MongoClient(MongoClientSettings.FromConnectionString(ConnectionString));
+
+            Debug.Log("Initialized!");
+        }
+    }
+}
